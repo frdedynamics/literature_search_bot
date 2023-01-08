67edc1738c102b45134107c85221c61f30fa64f3
fjnn <gizem.ates@hvl.no>
2023-01-08 15:25:16 +0100

    Removed dummy text

--- scripts/google_scholar_search.py
+++ scripts/google_scholar_search.py
@@ -35,12 +35,6 @@ class Search:
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-        # self.driver = webdriver.Chrome()
-
-        #print(search_term[1:])
-        #search_term = "https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&q="+self.search_terms_exact+"t&btnG="
-        #"https://scholar.google.com/scholar?hl=en&scisbd=1&as_sdt=0%2C5&q=allintitle%3A+interaction+OR+cooperation+OR+collaboration+%22human+robot%22+&btnG="
-        #search_term = "https://scholar.google.com/scholar?as_q=&as_epq="+self.search_terms_exact+"&as_oq="+self.search_terms_at_least_one+"&as_eq=&as_occt=any&as_sauthors=&as_publication=&as_ylo="+self.search_year+"&as_yhi="+self.search_year+"&hl=en&as_sdt=0%2C5"
         
         if mode == 0: # without
             search_term = "https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&as_ylo=2017&as_yhi=2017&q=%22"+self.search_terms_exact+"%22+AND"+self.search_terms_at_least_one+"&as_eq=&as_occt=any&as_sauthors=&as_publication=&as_ylo="+self.search_year+"&as_yhi="+self.search_year+"&hl=en&as_sdt=0%2C5"
@@ -120,12 +114,12 @@ if __name__ == "__main__":
     search_terms_at_least_one = ['interaction', 'cooperation', 'collaboration']
     search_years = np.arange(2013, 2023)
 
-    # # ------------ I am not a robot ------------ # #
+    # # ------------Uncomment here if "I am not a robot" occurs. Might need VPN ------------ # #
     # search_year = '2020'
     # my_search = Search(search_terms_exact, search_terms_at_least_one, search_year)
     # my_search.robot_manual_check()
    
-    # # ------------ Redo the search ------------ # #
+    # # ------------ Uncomment here to redo the search ------------ # #
     # # max 10 years otherwise bot recognition
     # df = pd.DataFrame(columns=[["year", "found","found_camera", "found_imu"]])
     # # df = Search.test_df(df)
@@ -152,30 +146,3 @@ if __name__ == "__main__":
     
     Search.plot(df)
     print(df.tail(10))
-
-
-
-
-    # labels = ['G1', 'G2', 'G3', 'G4', 'G5']
-    # men_means = [20, 35, 30, 35, 27]
-    # women_means = [25, 32, 34, 20, 25]
-    # men_std = [2, 3, 4, 1, 2]
-    # women_std = [3, 5, 2, 3, 3]
-    # width = 0.35       # the width of the bars: can also be len(x) sequence
-
-    # fig, ax = plt.subplots()
-
-    # ax.bar(labels, men_means, width, yerr=men_std, label='Men')
-    # ax.bar(labels, women_means, width, yerr=women_std, bottom=men_means,
-    #     label='Women')
-    
-    # ax. annotate('Starting', xy =('G1', 50),
-    #          xytext =(('G1', 50)),
-    #          arrowprops = dict(facecolor ='green',
-    #                            shrink = 0.05),   )
-
-    # ax.set_ylabel('Scores')
-    # ax.set_title('Scores by group and gender')
-    # ax.legend()
-
-    # plt.show()
